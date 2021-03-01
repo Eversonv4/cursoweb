2286b7e15a7fe84bc85fc6eb030eb484df45313e
Everson Soares <eversonv4@gmail.com>
2021-03-01 06:38:10 -0300

    Update testando.js

--- exercicios-js/testando.js
+++ exercicios-js/testando.js
@@ -1,4 +1,4 @@
 console.log('Estou aprendendo java espírito')
 console.log('a multiplicação entre 2 e 8 é:',2 * 8)
-console.log('Quem é o girombilson da mamãe?')
-console.log('EUUUUUU KKK')
+console.log('Um, Dois, Três... Testando')
+console.log('Eita, pleura!')
